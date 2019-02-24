aa2bfdb1c5d30ec3f82113744888f5fcc29db40b
Kevinjob99 <kevinjobi6@gmail.com>
2019-02-25 00:33:52 +0100

    Added the session query and specs

---
+++ apps/keepadoo/src/app/state/session.query.spec.ts
@@ -0,0 +1,63 @@
+import { testUser } from '../../test-utilities/test-objects';
+import { SessionQuery } from './session.query';
+import { SessionStore } from './session.store';
+
+describe('SessionQuery', () => {
+  let query: SessionQuery;
+  const store = new SessionStore();
+
+  beforeEach(() => {
+    query = new SessionQuery(store);
+  });
+
+  it('should create an instance', () => {
+    expect(query).toBeTruthy();
+  });
+
+  describe('isLoggedIn$', () => {
+    it('should return true if there is a user', done => {
+      store.update({ user: testUser });
+
+      query.isLoggedIn$.subscribe((data: boolean) => {
+        expect(data).toBe(true);
+        done();
+      });
+    });
+
+    it('should return false if there is no user', done => {
+      store.update({ user: null });
+
+      query.isLoggedIn$.subscribe((data: boolean) => {
+        expect(data).toBe(false);
+        done();
+      });
+    });
+  });
+
+  describe('loggedInUser$', () => {
+    it('should return the logged in user full name', done => {
+      store.update({ user: testUser });
+
+      query.loggedInUser$.subscribe((data: string) => {
+        expect(data).toBe(`${testUser.firstName} ${testUser.lastName}`);
+        done();
+      });
+    });
+  });
+
+  describe('isLoggedIn', () => {
+    it('should return true if the user is logged in', () => {
+      store.update({ user: testUser });
+
+      const result = query.isLoggedIn();
+      expect(result).toBe(true);
+    });
+
+    it('should return false if the user is not logged in', () => {
+      store.update({ user: null });
+
+      const result = query.isLoggedIn();
+      expect(result).toBe(false);
+    });
+  });
+});
---
+++ apps/keepadoo/src/app/state/session.query.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Query, toBoolean } from '@datorama/akita';
+import { filter, map } from 'rxjs/operators';
+import { SessionState } from './models/session-state';
+import { SessionStore } from './session.store';
+
+@Injectable({ providedIn: 'root' })
+export class SessionQuery extends Query<SessionState> {
+  isLoggedIn$ = this.select(({ user }) => toBoolean(user));
+
+  loggedInUser$ = this.select().pipe(
+    filter(({ user }) => toBoolean(user)),
+    map(({ user: { firstName, lastName } }) => `${firstName} ${lastName}`)
+  );
+
+  constructor(protected store: SessionStore) {
+    super(store);
+  }
+
+  isLoggedIn(): boolean {
+    return toBoolean(this.getValue().user);
+  }
+}
