bf335d6eafe3bbdd3225778f3aac1cb547f98e73
Kevinjob99 <kevinjobi6@gmail.com>
2019-02-25 23:47:22 +0100

    Added basic test

--- apps/keepadoo/src/app/app.component.spec.ts
+++ apps/keepadoo/src/app/app.component.spec.ts
@@ -9,4 +9,10 @@ describe('AppComponent', () => {
       declarations: [AppComponent]
     }).compileComponents();
   }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  });
 });
