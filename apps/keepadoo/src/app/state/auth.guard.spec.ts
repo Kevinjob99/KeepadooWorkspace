e75ce57dd52353c7f9e37f504dc040a014b896b5
Kevinjob99 <kevinjobi6@gmail.com>
2019-03-02 23:54:05 +0100

    The AuthGuard now sets the redirectUrl

--- apps/keepadoo/src/app/state/auth.guard.spec.ts
+++ apps/keepadoo/src/app/state/auth.guard.spec.ts
@@ -1,24 +1,32 @@
 import { TestBed } from '@angular/core/testing';
 import { Router } from '@angular/router';
-import { routerMock, sessionQueryMock } from '../../test-utilities/test-mocks';
+import {
+  routerMock,
+  sessionQueryMock,
+  sessionStoreMock
+} from '../../test-utilities/test-mocks';
 import { AuthGuard } from './auth.guard';
 import { SessionQuery } from './session.query';
+import { SessionStore } from './session.store';
 
 describe('AuthGuard', () => {
   let guard: AuthGuard;
   let query: SessionQuery;
+  let store: SessionStore;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
         AuthGuard,
         { provide: SessionQuery, useValue: sessionQueryMock },
+        { provide: SessionStore, useValue: sessionStoreMock },
         { provide: Router, useValue: routerMock }
       ]
     });
 
     guard = TestBed.get(AuthGuard);
     query = TestBed.get(SessionQuery);
+    store = TestBed.get(SessionStore);
   });
 
   it('should return true if the user is logged in', () => {
@@ -28,19 +36,30 @@ describe('AuthGuard', () => {
     expect(result).toBe(true);
   });
 
-  it('should return false if the user is not logged in', () => {
-    jest.spyOn(query, 'isLoggedIn').mockReturnValue(false);
+  describe('NotLoggedIn', () => {
+    beforeEach(function() {
+      jest.spyOn(query, 'isLoggedIn').mockReturnValue(false);
+    });
 
-    const result = guard.canActivate();
-    expect(result).toBe(false);
-  });
+    it('should return false if the user is not logged in', () => {
+      const result = guard.canActivate();
+      expect(result).toBe(false);
+    });
 
-  it('should redirect to login if the user is not logged in', () => {
-    const router: Router = TestBed.get(Router);
-    jest.spyOn(query, 'isLoggedIn').mockReturnValue(false);
+    it('should redirect to login if the user is not logged in', () => {
+      const router: Router = TestBed.get(Router);
 
-    guard.canActivate();
+      guard.canActivate();
 
-    expect(router.navigateByUrl).toHaveBeenCalledWith('/login');
+      expect(router.navigateByUrl).toHaveBeenCalledWith('/login');
+    });
+
+    it('should set the redirectUrl in the store', () => {
+      guard.canActivate();
+
+      expect(store.update).toHaveBeenCalledWith({
+        redirectUrl: routerMock.url
+      });
+    });
   });
 });
--- apps/keepadoo/src/app/state/auth.guard.ts
+++ apps/keepadoo/src/app/state/auth.guard.ts
@@ -1,17 +1,23 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 import { SessionQuery } from './session.query';
+import { SessionStore } from './session.store';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  constructor(private sessionQuery: SessionQuery, private router: Router) {}
+  constructor(
+    private sessionQuery: SessionQuery,
+    private sessionStore: SessionStore,
+    private router: Router
+  ) {}
 
   canActivate(): boolean {
     const isUserLoggedIn = this.sessionQuery.isLoggedIn();
 
     if (!isUserLoggedIn) {
+      this.sessionStore.update({ redirectUrl: this.router.url });
       this.router.navigateByUrl('/login');
       return false;
     }
