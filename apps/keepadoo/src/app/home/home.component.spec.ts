2d3ae2c57daca135076fa9d757ca67937e3d61bd
Kevinjob99 <kevinjobi6@gmail.com>
2019-03-03 21:06:29 +0100

    Added basic structure for the home component

--- apps/keepadoo/src/app/home/home.component.spec.ts
+++ apps/keepadoo/src/app/home/home.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HomeComponent } from './home.component';
 
@@ -21,5 +22,31 @@ describe('HomeComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('Render', () => {
+    it('should have a link for the movies feature', () => {
+      const movieFeatureLink = fixture.debugElement.queryAll(
+        By.css('.movies-link')
+      );
+
+      expect(movieFeatureLink.length).toBe(1);
+    });
+
+    it('should have a link for the tv-shows feature', () => {
+      const movieFeatureLink = fixture.debugElement.queryAll(
+        By.css('.tv-shows-link')
+      );
+
+      expect(movieFeatureLink.length).toBe(1);
+    });
+
+    it('should have a link for the places feature', () => {
+      const movieFeatureLink = fixture.debugElement.queryAll(
+        By.css('.places-link')
+      );
+
+      expect(movieFeatureLink.length).toBe(1);
+    });
   });
 });
