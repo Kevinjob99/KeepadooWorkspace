b2b273feaeb3c69d1531e4480710207ec0c69552
Kevinjob99 <kevinjobi6@gmail.com>
2019-03-03 21:45:33 +0100

    MoviesLists module genesis

---
+++ apps/keepadoo/src/app/movies-lists/movies-lists.module.ts
@@ -0,0 +1,9 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MoviesListsRoutingModule } from './movies-lists-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, MoviesListsRoutingModule]
+})
+export class MoviesListsModule {}
--- apps/keepadoo/src/app/movies/movies.module.ts
+++
@@ -1,9 +0,0 @@
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
-import { MoviesRoutingModule } from './movies-routing.module';
-
-@NgModule({
-  declarations: [],
-  imports: [CommonModule, MoviesRoutingModule]
-})
-export class MoviesModule {}
