b25da482d87ac58f3deca3f84593bf264c0f4f6d
Kevinjob99 <kevinjobi6@gmail.com>
2019-03-09 14:45:19 +0100

    Basic setup for the movies-lists component

--- apps/keepadoo/src/app/movies-lists/movies-lists/movies-lists.component.ts
+++ apps/keepadoo/src/app/movies-lists/movies-lists/movies-lists.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { MoviesList } from '../state/models/movies-list';
+import { MoviesListsQuery } from '../state/movies-lists.query';
+import { MoviesListsService } from '../state/movies-lists.service';
 
 @Component({
   selector: 'keepadoo-movies-lists',
@@ -6,7 +10,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movies-lists.component.scss']
 })
 export class MoviesListsComponent implements OnInit {
-  constructor() {}
+  moviesLists$: Observable<MoviesList[]>;
+
+  constructor(
+    moviesListsQuery: MoviesListsQuery,
+    moviesListsService: MoviesListsService
+  ) {
+    this.moviesLists$ = moviesListsQuery.selectAll();
+  }
 
   ngOnInit() {}
 }
