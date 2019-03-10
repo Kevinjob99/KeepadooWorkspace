10e1998e8f852887eef8e60a4dcaeaed9e79b5d1
Kevinjob99 <kevinjobi6@gmail.com>
2019-03-10 18:45:28 +0100

    Added movie interface

---
+++ apps/keepadoo/src/app/movies-lists/movies/state/models/movie.ts
@@ -0,0 +1,11 @@
+export interface Movie {
+  id: string;
+  genre_ids: number[];
+  listId: string;
+  overview: string;
+  poster_path: string;
+  release_date: Date;
+  title: string;
+  vote_average: number;
+  vote_count: number;
+}
