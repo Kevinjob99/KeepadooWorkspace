c357c4dac6136190ebf5e12418597602033c87ce
Kevinjob99 <kevinjobi6@gmail.com>
2019-03-10 18:45:39 +0100

    Added testMovies object

--- apps/keepadoo/src/test-utilities/test-objects.ts
+++ apps/keepadoo/src/test-utilities/test-objects.ts
@@ -1,4 +1,5 @@
 import { User as FirebaseUser } from 'firebase';
+import { Movie } from '../app/movies-lists/movies/state/models/movie';
 import { MoviesList } from '../app/movies-lists/state/models/movies-list';
 import { User } from '../app/state/models/user';
 
@@ -24,5 +25,41 @@ export const testMoviestLists: MoviesList[] = [
     id: '2',
     name: 'seen',
     userId: 'joker'
+  }
+];
+
+export const testMovies: Movie[] = [
+  {
+    id: '1',
+    genre_ids: [1],
+    listId: '1',
+    overview: 'I am batman',
+    poster_path: 'batman-poster-path',
+    release_date: new Date(),
+    title: 'Batman begins',
+    vote_average: 5,
+    vote_count: 20033
+  },
+  {
+    id: '2',
+    genre_ids: [1],
+    listId: '1',
+    overview: 'I am batman again',
+    poster_path: 'batman-second-poster-path',
+    release_date: new Date(),
+    title: 'Batman continues',
+    vote_average: 4.9,
+    vote_count: 2433
+  },
+  {
+    id: '3',
+    genre_ids: [1],
+    listId: '2',
+    overview: 'I am joker',
+    poster_path: 'joker-poster-path',
+    release_date: new Date(),
+    title: 'Joker begins',
+    vote_average: 4.6,
+    vote_count: 343
   }
 ];
