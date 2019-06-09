50ace28304f5d556bf805c851863b7175e6a461c
Kevinjob99 <kevinjobi6@gmail.com>
2019-06-09 13:58:48 +0200

    Implemented movie search

--- apps/keepadoo/src/app/movies-lists/movie-search/state/movie-search.service.spec.ts
+++ apps/keepadoo/src/app/movies-lists/movie-search/state/movie-search.service.spec.ts
@@ -1,11 +1,18 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { TestBed } from '@angular/core/testing';
+import { HttpRequest } from '@angular/common/http';
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from '@angular/common/http/testing';
+import { inject, TestBed } from '@angular/core/testing';
+import { environment } from '../../../../environments/environment';
+import { MovieSearchResult } from './models/movie-search-results';
 import { MovieSearchService } from './movie-search.service';
 import { MovieSearchStore } from './movie-search.store';
 
 describe('MovieSearchService', () => {
   let movieSearchService: MovieSearchService;
   let movieSearchStore: MovieSearchStore;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -15,9 +22,104 @@ describe('MovieSearchService', () => {
 
     movieSearchService = TestBed.get(MovieSearchService);
     movieSearchStore = TestBed.get(MovieSearchStore);
+    httpTestingController = TestBed.get(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(movieSearchService).toBeDefined();
+  });
+
+  describe('searchMovies', () => {
+    it('should get movie search results from tmdb and put them in the store', inject(
+      [MovieSearchService],
+      (service: MovieSearchService) => {
+        const searchText = 'Thor';
+        const dataToReturn = {
+          results: [
+            {
+              title: 'Thor',
+              overview: 'Something about a greek god..'
+            } as MovieSearchResult,
+            {
+              title: 'Iron man',
+              overview: 'Something about a dude with an iron suit..'
+            } as MovieSearchResult
+          ]
+        };
+
+        service.searchMovies(searchText).subscribe();
+
+        const req = httpTestingController.expectOne(
+          (request: HttpRequest<any>) => {
+            const containsUrl = request.url.includes(
+              environment.tmdbConfig.apiUrl
+            );
+            const containsKey =
+              request.params.get('api_key') === environment.tmdbConfig.api_key;
+            const containesSearchQuery =
+              request.params.get('query') === searchText;
+            return containsUrl && containsKey && containesSearchQuery;
+          }
+        );
+
+        expect(req.request.method).toEqual('GET');
+
+        req.flush(dataToReturn);
+        httpTestingController.verify();
+      }
+    ));
+
+    it('should filter movie search results without a poster from tmdb', (done: any) => {
+      inject([MovieSearchService], (service: MovieSearchService) => {
+        const searchText = 'Thor';
+        const dataToReturn = {
+          results: [
+            {
+              title: 'Thor',
+              overview: 'Something about a greek god..',
+              poster_path: 'thor-poster'
+            } as MovieSearchResult,
+            {
+              title: 'Iron man',
+              overview: 'Something about a dude with an iron suit..',
+              poster_path: 'iron-man-poster'
+            } as MovieSearchResult,
+            {
+              title: 'Chinese Iron Man',
+              overview: 'Something about a chinese dude with an iron suit..',
+              poster_path: ''
+            } as MovieSearchResult
+          ]
+        };
+
+        service
+          .searchMovies(searchText)
+          .subscribe((searchResults: MovieSearchResult[]) => {
+            expect(searchResults).toEqual([
+              dataToReturn.results[0],
+              dataToReturn.results[1]
+            ]);
+            done();
+          });
+
+        const req = httpTestingController.expectOne(
+          (request: HttpRequest<any>) => {
+            const containsUrl = request.url.includes(
+              environment.tmdbConfig.apiUrl
+            );
+            const containsKey =
+              request.params.get('api_key') === environment.tmdbConfig.api_key;
+            const containesSearchQuery =
+              request.params.get('query') === searchText;
+            return containsUrl && containsKey && containesSearchQuery;
+          }
+        );
+
+        expect(req.request.method).toEqual('GET');
+
+        req.flush(dataToReturn);
+        httpTestingController.verify();
+      })();
+    });
   });
 });
--- apps/keepadoo/src/app/movies-lists/movie-search/state/movie-search.service.ts
+++ apps/keepadoo/src/app/movies-lists/movie-search/state/movie-search.service.ts
@@ -1,6 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ID } from '@datorama/akita';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { environment } from '../../../../environments/environment';
 import { MovieSearchResult } from './models/movie-search-results';
 import { MovieSearchStore } from './movie-search.store';
 
@@ -15,6 +18,24 @@ export class MovieSearchService {
     this.http
       .get('https://akita.com')
       .subscribe(entities => this.movieSearchStore.set(entities));
+  }
+
+  searchMovies(text: string): Observable<MovieSearchResult[]> {
+    const urlToUse = `${environment.tmdbConfig.apiUrl}/search/movie`;
+    const params = new HttpParams()
+      .set('api_key', environment.tmdbConfig.api_key)
+      .set('query', text);
+
+    return this.http.get(urlToUse, { params }).pipe(
+      map((response: any) => {
+        return response.results as MovieSearchResult[];
+      }),
+      map((data: MovieSearchResult[]) => {
+        return data.filter((movieSearchResult: MovieSearchResult) => {
+          return !!movieSearchResult.poster_path;
+        });
+      })
+    );
   }
 
   add(movieSearch: MovieSearchResult) {
