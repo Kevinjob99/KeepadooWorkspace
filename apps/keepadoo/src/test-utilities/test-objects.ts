2ad3383a7e65dbed842f281ee9818f9dc4be880f
Kevinjob99 <kevinjobi6@gmail.com>
2019-06-26 16:42:31 +0200

    Added testMovieSearchResults

--- apps/keepadoo/src/test-utilities/test-objects.ts
+++ apps/keepadoo/src/test-utilities/test-objects.ts
@@ -1,4 +1,5 @@
 import { User as FirebaseUser } from 'firebase';
+import { MovieSearchResult } from '../app/movies-lists/movie-search/state/models/movie-search-results';
 import { Movie } from '../app/movies-lists/movies/state/models/movie';
 import { MoviesList } from '../app/movies-lists/state/models/movies-list';
 import { User } from '../app/state/models/user';
@@ -100,5 +101,56 @@ export const testMovies: Movie[] = [
     title: 'Joker begins',
     vote_average: 4.6,
     vote_count: 343
+  }
+];
+
+export const testMovieSearchResults: MovieSearchResult[] = [
+  {
+    id: 1,
+    genre_ids: [1],
+    overview: 'I am batman',
+    poster_path: 'batman-poster-path',
+    title: 'Batman begins',
+    vote_average: 5,
+    vote_count: 20033,
+    adult: false,
+    backdrop_path: 'batman-backdrop-path',
+    original_language: 'en',
+    original_title: 'I am batman',
+    popularity: 3432,
+    release_date: '2342423',
+    video: false
+  },
+  {
+    id: 2,
+    genre_ids: [1],
+    overview: 'I am batman again',
+    poster_path: 'batman-second-poster-path',
+    title: 'Batman continues',
+    vote_average: 4.9,
+    vote_count: 2433,
+    adult: false,
+    backdrop_path: 'batman-second-backdrop-path',
+    original_language: 'ro',
+    original_title: 'Batman continues',
+    popularity: 4524422,
+    release_date: '432',
+    video: true
+  },
+  {
+    id: 3,
+    genre_ids: [1],
+    overview: 'I am joker',
+    poster_path: 'joker-poster-path',
+    title: 'Joker begins',
+    vote_average: 4.6,
+    vote_count: 343,
+    adult: false,
+    backdrop_path: 'joker-backdrop-path',
+    original_language: 'nl',
+    original_title: 'Joker begins',
+    popularity: 11,
+    release_date: '8778989',
+    video: false
   }
 ];
