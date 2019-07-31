830ed02e8b1aa1d84d261c67e2a29bd44b4ab0e5
Kevinjob99 <kevinjobi6@gmail.com>
2019-07-31 22:29:23 +0200

    Added editMode property to the MoviesStore

--- apps/keepadoo/src/app/movies-lists/movies/state/movies.store.ts
+++ apps/keepadoo/src/app/movies-lists/movies/state/movies.store.ts
@@ -2,12 +2,14 @@ import { Injectable } from '@angular/core';
 import { EntityState, EntityStore, StoreConfig } from '@datorama/akita';
 import { Movie } from './models/movie';
 
-export interface MoviesState extends EntityState<Movie> {}
+export interface MoviesState extends EntityState<Movie> {
+  editMode: boolean;
+}
 
 @Injectable()
 @StoreConfig({ name: 'movies' })
 export class MoviesStore extends EntityStore<MoviesState, Movie> {
   constructor() {
-    super();
+    super({ editMode: false });
   }
 }
