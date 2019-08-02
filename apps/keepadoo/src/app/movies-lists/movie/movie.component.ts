99fc6cc52664769f512c9977bfeba9119db8ec33
Kevinjob99 <kevinjobi6@gmail.com>
2019-08-02 20:40:06 +0200

    Added wiggle animation

--- apps/keepadoo/src/app/movies-lists/movie/movie.component.ts
+++ apps/keepadoo/src/app/movies-lists/movie/movie.component.ts
@@ -15,9 +15,17 @@ import { Movie } from '../movies/state/models/movie';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MovieComponent implements OnInit {
+  readonly wiggleClasses = ['wiggle1', 'wiggle2'];
+
   @Input() editMode = false;
   @Input() movie: Movie;
   @Output() delete = new EventEmitter<Movie>();
+
+  wiggleStyle = this.wiggleClasses[
+    Math.floor(Math.random() * this.wiggleClasses.length)
+  ];
+  animationDelay = `${this.generateRandomNumber(-0.75, -0.15)}s`;
+  animationDuration = `${this.generateRandomNumber(0.2, 0.35)}s`;
 
   constructor() {}
 
@@ -25,5 +33,9 @@ export class MovieComponent implements OnInit {
 
   onDelete(): void {
     this.delete.emit(this.movie);
+  }
+
+  private generateRandomNumber(min: number, max: number): string {
+    return (Math.random() * (max - min) + min).toFixed(2);
   }
 }
