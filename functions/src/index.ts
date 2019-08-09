e71b206b5ae11eb5d2f79eb2e2199e7fb87b1411
Kevinjob99 <kevinjobi6@gmail.com>
2019-08-09 12:36:07 +0200

    Hook into movie-write event

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,8 @@
 import * as functions from 'firebase-functions';
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
-
-export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send('Hello from Firebase!');
-});
+exports.aggregateMoviesInList = functions.firestore
+  .document('movies/{movieId}')
+  .onWrite((snapshot, context) => {
+    const movieId = context.params.movieId;
+    console.log('Movie written', movieId);
+  });
