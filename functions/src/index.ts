aa3a4f684031ef04109fc1b2dc2e2da542576907
Kevinjob99 <kevinjobi6@gmail.com>
2019-08-09 23:58:49 +0200

    Added functions for listening to Create and Delete events for Movie

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,49 @@
+import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
 
-exports.aggregateMoviesInList = functions.firestore
+admin.initializeApp();
+
+exports.aggregateMoviesInListOnCreate = functions.firestore
   .document('movies/{movieId}')
-  .onWrite((snapshot, context) => {
-    const movieId = context.params.movieId;
-    console.log('Movie written', movieId);
+  .onCreate(async (snapshot, context) => {
+    const listId = (snapshot.data() as any).listId;
+
+    return await aggregateMoviesInList(listId);
   });
+
+exports.aggregateMoviesInListOnDelete = functions.firestore
+  .document('movies/{movieId}')
+  .onDelete(async (snapshot, context) => {
+    const listId = (snapshot.data() as any).listId;
+
+    return await aggregateMoviesInList(listId);
+  });
+
+async function aggregateMoviesInList(listId: string) {
+  console.log('listId', listId);
+  const movies = await admin
+    .firestore()
+    .collection('movies')
+    .where('listId', '==', listId)
+    .orderBy('added_on', 'desc')
+    .get();
+
+  // get the total comment count
+  const moviesCount = movies.size;
+  console.log('moviesCount', moviesCount);
+
+  const recentMovies: any[] = [];
+  // add data from the 5 most recent movies to the array
+  movies.forEach(doc => {
+    recentMovies.push(doc.data());
+  });
+  recentMovies.splice(5);
+  console.log('recent movies', recentMovies);
+
+  // run update
+  const list = admin
+    .firestore()
+    .collection('movies-lists')
+    .doc(listId);
+  return list.update({ moviesCount, recentMovies });
+}
